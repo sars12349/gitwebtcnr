542a5b4d68838418a9e1352eea5277bcd1b500f7
chunyenchang <sars12349@gmail.com>
2019-01-03 15:33:50 +0800

    AJAX和PHP  簡單教學練習

---
+++ HTC/2019010302.php
@@ -0,0 +1,5 @@
+<?php
+	header("Access-Control-Allow-Origin:*");
+	echo'<img src="http://192.168.60.121/HTC/image/htc.jpg" alt="">'
+?>
+
---
+++ HTC/ajax02.php
@@ -0,0 +1,18 @@
+<?php
+	header("Access-Control-Allow-Origin:*");
+	//echo '由PHP程式傳回的資料: '.$_POST['msn'] .$_POST['msh'] .$_POST['msw'];
+	//msn姓名,msh身高,msw體重
+	//$msw=msw;
+	//$msh=msh;
+	//$bmi=$msw/(($msh/100)^2);
+	$bmi=($_POST['msw']/(($_POST['msh']/100)^2));
+	if($bmi>25)
+	{
+		echo '姓名'.$_POST['msn'].' BMI='.$bmi.' 你太胖了';
+
+	}else
+	{
+		echo '姓名'.$_POST['msn'].' BMI='.$bmi.' 身材不錯';
+	}
+
+?>
